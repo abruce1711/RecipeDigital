25ef33ddc224ab82044441195340944e9459060b
Andrew Bruce <abruce1711@gmail.com>
2018-07-22 23:18:35 +0100

    Added mobile navigation

--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -1,3 +1,8 @@
+var clicks = 0;
+
+document.addEventListener("turbolinks:load", function () {
+    clicks = 0;
+});
 
 function edit_step(id) {
     document.getElementById("edit_step_" + id).style.display = "block";
@@ -21,4 +26,26 @@ function cancel_ingredient_edit(id) {
     document.getElementById("edit_ingredient_" + id).style.display = "none";
     document.getElementById("ingredient_" + id).style.display = "inline";
     document.getElementById("ingredient_buttons_" + id).style.display = "inline";
+}
+
+function decide(x) {
+    if (clicks == 0) {
+        document.getElementById("mobile_nav").style.visibility = "visible";
+        document.getElementById("mobile_nav").style.opacity = "1";
+        document.getElementById("mobile_nav").style.zIndex = "5";
+        document.getElementById("bar1").style.border = "none";
+        document.getElementById("bar3").style.border = "none";
+        x.classList.toggle("change");
+        clicks = 1;
+    }
+
+    else if (clicks == 1) {
+        document.getElementById("mobile_nav").style.visibility = "hidden";
+        document.getElementById("mobile_nav").style.opacity = "0";
+        document.getElementById("mobile_nav").style.zIndex = "0";
+        document.getElementById("bar1").style.border = "1px solid black";
+        document.getElementById("bar3").style.border = "1px solid black";
+        x.classList.toggle("change");
+        clicks = 0;
+    }
 }
--- app/models/step.rb
+++ app/models/step.rb
@@ -4,6 +4,14 @@ class Step < ApplicationRecord
   validates_presence_of :content
   validate :capitalize
 
+#  def self.move_step_up(steps, index)
+#    moved_up = steps[index]
+#    moved_down = steps[index-1]
+#    moved_up.content, moved_down.content = moved_down.content, moved_up.content
+#    moved_up.save
+#    moved_down.save
+#  end
+
   private
   def capitalize
     self.content = self.content.capitalize
