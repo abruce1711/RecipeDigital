ba7c8cdce5bfea360b49787ce1146ab259ad7c27
Andrew Bruce <abruce1711@gmail.com>
2018-08-01 23:48:13 +0100

    Part of the way there but it's currently not uploading properly.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,36 @@
 class UsersController < ApplicationController
 
+  before_action :set_user, only: [:edit, :update]
+
   def show
     @user = current_user
+    @recipes = Recipe.where(:user_id => @user.id)
+  end
+
+  def edit
+    check_user
+  end
+
+  def update
+    check_user
+    @user.update(account_update_params)
+    pp @user.errors.full_messages
+    redirect_to @user
+  end
+
+  private
+  def set_user
+    @user = User.find_by_slug(params[:id])
+  end
+
+  def account_update_params
+    params.require(:user).permit(:first_name, :last_name, :username, :email, :password, :current_password, :avatar)
+  end
+
+  def check_user
+    if @user.id != current_user.id
+      raise ActionController::RoutingError.new('Not Found')
+    end
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180801115944) do
+ActiveRecord::Schema.define(version: 20180801213624) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -69,6 +69,7 @@ ActiveRecord::Schema.define(version: 20180801115944) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "slug"
+    t.string   "avatar"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
     t.index ["username"], name: "index_users_on_username", unique: true, using: :btree
