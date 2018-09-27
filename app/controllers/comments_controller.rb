d9ef57a3711c51a4c7bae48d4fb48d825dc84094
Andrew Bruce <abruce1711@gmail.com>
2018-09-27 23:35:29 +0100

    Implemented adding comments to recipes

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,18 @@
 class CommentsController < ApplicationController
+
+  before_action :authenticate_user!, only: :create
+
+  def create
+    @comment = Comment.new(comment_params)
+    @recipe = Recipe.find(params[:comment][:recipe_id])
+    @comment.user_id = current_user.id
+    @comment.save
+    redirect_to @recipe
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:title, :body, :rating, :recipe_id, :user_id)
+  end
+
 end
